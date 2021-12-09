268b8330af930ee792b2c3c0fd14862012af7b42
Endriw-Ventura <endriwventura@yahoo.com>
2021-12-09 12:36:35 -0300

    Poatek teste finished

--- lib/admin_page.dart
+++ lib/admin_page.dart
@@ -42,122 +42,121 @@ class _AdminPageState extends State<AdminPage> {
         title: Text('Test Three - Classes - Admin Page',
         ),
       ),
-      body: Column(
-        children: [
-          Container(
-            color: Colors.purple,
-            width: MediaQuery
-                .of(context)
-                .size
-                .width,
-            height: 100,
-            child:
-            Row(
-              children: [
-                Text('Select the new type of fuel: ',
-                  style: TextStyle(
-                    fontSize: 18,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 30),
-                  child: DropdownButton(
-                    value: selected,
-                    icon: const Icon(Icons.arrow_downward),
-                    iconSize: 24,
-                    elevation: 16,
-                    style: const TextStyle(color: Colors.black),
-                    underline: Container(
-                      height: 2,
-                      color: Colors.deepPurpleAccent,
+      body: Stack(
+        children: <Widget>[Column(
+          children: [
+            Container(
+              width: MediaQuery
+                  .of(context)
+                  .size
+                  .width,
+              height: 100,
+              child:
+              Row(
+                children: [
+                  Text('Select the new type of fuel: ',
+                    style: TextStyle(
+                      fontSize: 18,
                     ),
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        selected = newValue!;
-                      });
-                    },
-                    items: dropdownItemsAux,
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30),
+                    child: DropdownButton(
+                      value: selected,
+                      icon: const Icon(Icons.arrow_downward),
+                      iconSize: 24,
+                      elevation: 16,
+                      style: const TextStyle(color: Colors.black),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.deepPurpleAccent,
+                      ),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          selected = newValue!;
+                        });
+                      },
+                      items: dropdownItemsAux,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Container(
-            color: Colors.red,
-            width: MediaQuery
-                .of(context)
-                .size
-                .width,
-            height: 100,
-          ),
-          Container(
-            color: Colors.blue,
-            width: MediaQuery
-                .of(context)
-                .size
-                .width,
-            height: 100,
-            child:
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    width: MediaQuery
-                        .of(context)
-                        .size
-                        .width / 1.2,
-                    child: Form(
-                      key: _formKey,
-                      onChanged: () =>
-                          setState(() =>
-                          _enableBtn = _formKey.currentState!.validate()),
-                      child: TextFormField(
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            txtValue = 'Enter a valid value';
-                            return txtValue;
-                          }
-                        },
-                        controller: _priceController,
-                        inputFormatters: <TextInputFormatter>[
-                          FilteringTextInputFormatter.allow(
-                            RegExp(r'^[0-9]{0,3}[.]{0,1}[0-9]{0,2}'),
+            Container(
+              width: MediaQuery
+                  .of(context)
+                  .size
+                  .width,
+              height: 100,
+            ),
+            Container(
+              width: MediaQuery
+                  .of(context)
+                  .size
+                  .width,
+              height: 100,
+              child:
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: MediaQuery
+                          .of(context)
+                          .size
+                          .width / 1.2,
+                      child: Form(
+                        key: _formKey,
+                        onChanged: () =>
+                            setState(() =>
+                            _enableBtn = _formKey.currentState!.validate()),
+                        child: TextFormField(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              txtValue = 'Enter a valid value';
+                              return txtValue;
+                            }
+                          },
+                          controller: _priceController,
+                          inputFormatters: <TextInputFormatter>[
+                            FilteringTextInputFormatter.allow(
+                              RegExp(r'^[0-9]{0,3}[.]{0,1}[0-9]{0,2}'),
+                            ),
+                          ],
+                          keyboardType: TextInputType.number,
+                          obscureText: false,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: txtValue,
                           ),
-                        ],
-                        keyboardType: TextInputType.number,
-                        obscureText: false,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: txtValue,
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            color: Colors.green,
-            width: MediaQuery
-                .of(context)
-                .size
-                .width,
-            height: 100,
-            child: ElevatedButton(onPressed: _enableBtn ?
-                () {
-              price = _priceController.text.toString();
-              double val = double.parse(price);
-              setPrice(selected, val, pump);
-              Navigator.pop(context, pump);
-            } : null,
-              child: Text(
-                  'Submit'
+                ],
               ),
             ),
-          ),
+            Container(
+              width: MediaQuery
+                  .of(context)
+                  .size
+                  .width / 4,
+              height: 50,
+              child: ElevatedButton(onPressed: _enableBtn ?
+                  () {
+                price = _priceController.text.toString();
+                double val = double.parse(price);
+                setPrice(selected, val, pump);
+                Navigator.pop(context, pump);
+              } : null,
+                child: Text(
+                    'Submit'
+                ),
+              ),
+            ),
+          ],
+        ),
         ],
       ),
     );
